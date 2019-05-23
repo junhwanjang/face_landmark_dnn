a51b5f99759d3009eec370491d8ceb14fae6bc0f
Junhwan Jang <junhwanjang@Junhwanui-MacBookPro.local>
2019-05-23 16:32:21 +0900

    smoothL1 metric 추가

---
+++ modeling/metrics.py
@@ -0,0 +1,10 @@
+from keras import backend as K
+
+# Our Own Loss Function
+HUBER_DELTA = 0.5
+def smoothL1(y_true, y_pred):
+    x = K.abs(y_true - y_pred)
+    x = K.switch(x < HUBER_DELTA, 0.5 * x ** 2, HUBER_DELTA * (x - 0.5 * HUBER_DELTA))
+    return  K.sum(x)
+
+
